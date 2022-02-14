14cdea538a9576beabb51a79f947d3c1af530637
Ztirom45 <91824822+Ztirom45@users.noreply.github.com>
2022-02-14 17:33:09 +0100

    Add files via upload

--- spacefighter/function.py
+++ spacefighter/function.py
@@ -46,11 +46,43 @@ def reset_player(file): #create random stars
     f = open(file+".dat","bw") #open file
     fellts = 104
     dist=22*3
-    a = int(fellts/10)
-    b = [ [[x*dist,y*dist],0,1] for y in range(10) for x in range(a)]
-    for y in range(fellts-a*10):b.append([[y*dist,a*dist],0,1])
-    b[len(b)-1][2] = 4
-    dump([[0,0,90,10],["void"],b],f)#player pos, angele iteam list, inventory
+    a = int(fellts/10)  #  [x,y],  frame costume count 
+    b = [ [[x*dist,y*dist],0,0,0] for y in range(10) for x in range(a)]
+    for y in range(fellts-a*10):b.append([[y*dist,a*dist],0,0,0])
+    b[len(b)-1][1] = 4
+    b[0][1] = 1
+    b[0][2] = 1
+    b[1][1] = 1
+    b[1][2] = 2
+    b[2][1] = 1
+    b[2][2] = 3
+    b[3][1] = 1
+    b[3][2] = 4
+    b[4][1] = 1
+    b[4][2] = 5
+    b[4][3] = 100
+    dump([[0,0,90,10],["FErit","Cabon","Oxygen","NAtrium","shot"],b],f)#player pos, angele iteam list, inventory
+    f.close() #write random stars in file
+    
+def save_space(file,a,b,d): #create random stars
+    f = open(file+".dat","bw") #open file
+    img_dict = load_img("image")
+    img_list = [
+        "astoroid1",
+        "astoroid2",
+        "astoroid3",]
+    for x in b:
+              c = img_dict[img_list[x[4]]]
+              c = pygame.transform.rotozoom(c,-x[1],x[3])
+              x.append(tuple(c.get_rect()))
+              
+    dump([a,b,d],f)
+    f.close() #write random stars in file
+    
+
+def save_player(file,a,b,iteam_list): #create random stars
+    f = open(file+".dat","bw") #open file
+    dump([[0,0,90,10],iteam_list,b],f)#player pos, angele iteam list, inventory
     f.close() #write random stars in file
 
 def load_file(file): #load all stars
@@ -59,4 +91,3 @@ def load_file(file): #load all stars
     f.close() #close file
     return l # gives list l return
 
-#reset_space("space")
